e54c2a286498228b7f7c8b585c7776ff4af50a21
yesez <jimmysanz5@hotmail.com>
2012-03-17 02:55:44 -0600

    Creada Demo para diferentes AppBars en base a los Panorama Items de una página

---
+++ DemoAppBarsDinamicos/DemoAppBarsDinamicos/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace DemoAppBarsDinamicos
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private string[] AppBars = new string[]
+        {
+            "appbar1",
+            "appbar2",
+            "appbar3"
+        };
+
+        private void PanoramaPrinc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+            ApplicationBar = ((ApplicationBar)Application.Current.Resources[this.AppBars[this.PanoramaPrinc.SelectedIndex]]);
+        }
+    }
+}
