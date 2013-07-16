a88580eebcd6ac574eadf83782915d7385717f2d
Jimmy Sáenz <jsaenzr@outlook.com>
2013-07-15 22:05:24 -0600

    Items que ocupen todo el ancho del ListBox http://jsaenzr.com/blog/?p=1328

---
+++ AnchoListBoxItemsDemo/AnchoListBoxItemsDemo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.Phone.Controls;
+
+namespace AnchoListBoxItemsDemo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();   
+        }
+    }
+
+    public class ViewModel
+    {
+        public List<string> Personas { get; set; }
+
+        public ViewModel()
+        {
+            this.CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            Personas = new List<string>() { "Francesco Totti", "Franz Beckenbauer", "Juninho Pernambucano"};
+        }
+    }
+}
