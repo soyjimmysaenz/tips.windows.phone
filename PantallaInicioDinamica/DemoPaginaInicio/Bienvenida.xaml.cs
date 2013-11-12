9a8eb55141939927d1027c8c74ceae3b03c637f3
yesez5 <jsaenzr@outlook.com>
2013-11-12 14:44:36 -0600

    Agregar proyecto de 1ra pantalla dinámica para reorganizar los proyectos en el repositorio
    Crear config. de restauración de Nuget

---
+++ PantallaInicioDinamica/DemoPaginaInicio/Bienvenida.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace DemoPaginaInicio
+{
+    public partial class Bienvenida : PhoneApplicationPage
+    {
+        public Bienvenida()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIr_Click(object sender, EventArgs e) /**/
+        {
+            ISHelper.guardar(App.SALTAR_INTRO, App.SALTAR_INTRO);
+            NavigationService.Navigate(new Uri(App.MAINPAGE, UriKind.Relative));
+        }
+    }
+}
---
+++ PantallaInicioDinamica/DemoPaginaInicio/ISHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+
+namespace DemoPaginaInicio
+{
+    public class ISHelper
+    {
+        public static bool guardar(string llave, Object valor)
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+            bool respuesta = false;
+
+            if (settings.Contains(llave))
+            {
+                if (settings[llave] != valor)
+                {
+                    settings[llave] = valor;
+                }
+                respuesta = true;
+            }
+            else
+            {
+                settings.Add(llave, valor);
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+
+        public static bool existe(string llave)
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+            if (settings.Contains(llave)) return true; else return false;    
+        }
+    }
+}
